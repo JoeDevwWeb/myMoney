3fcb15f462342deb5bad5f78882e0db7784cd0c1
Joe Developer <101819884+JoeDevwWeb@users.noreply.github.com>
2023-07-10 19:16:47 -0300

    v0.3

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,11 +1,31 @@
-import '../index.css'
+import { FaAirbnb, FaBars} from "react-icons/fa";
+import style from './header.module.css'
 
 function Header(){
     return(
         <>
-            <h1>test</h1>
+            <header>
+                <div className={style.leftHeader}>
+                    <div className={style.logoHeader}>
+                        <FaAirbnb size="30px"/>
+                        <a href="/"><h1>Money.my</h1></a>
+                    </div>
+                    <div className={style.sideBar}>
+                        <div className={style.aside}>
+                            <ul>
+                                <li><FaBars /></li>
+                                <li><a href="">Visão geral</a></li>
+                                <li><a href="">Lançamentos</a></li>
+                                <li><a href="">Metas</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div className={style.rigtHeader}>
+                        <div className={style.perfilHeader}>perfil</div>
+                </div>
+            </header>
         </>
     )
 }
-
 export default Header
